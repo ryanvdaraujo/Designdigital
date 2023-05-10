454e2c088ff7a6ed4586ef39aaa8c1436cc95e5d
Ryan <negoryanaraujo@gmail.com>
2023-05-10 12:03:39 -0300

    Inicio do desenvolvimento do portifólio
    Criei o base e a estrutura de pastas com arquivos - menu e barra com nome e logo

--- src/app.py
+++ src/app.py
@@ -1,1 +1,22 @@
-#python
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
+
+@app.route("/projetos")
+def projetos():
+    return render_template("projetos.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
